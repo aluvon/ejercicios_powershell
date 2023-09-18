1e376ae1deea8db0d9fbdc652e645e98f5f7d70a
aluvon <aluvon5089@ieselcaminas.org>
2023-09-18 09:47:49 +0200

    practica dos

---
+++ ejercicio1.ps1
@@ -0,0 +1,5 @@
+﻿Clear-Host
+Write-Host "Hola $ENV:USERNAME"
+Write-Host "Tu directorio de trabajo es $env:HOMEPATH"
+Write-Host "Perteneces al dominio $env:USERDOMAIN"
+Write-Host "Tu equipo se llama $env:COMPUTERNAME"
---
+++ ejercicio3.ps1
@@ -0,0 +1,7 @@
+﻿Clear-Host
+Write-host "Hola, te pediré unos datos para calcular el salario a pagarte."
+[double]$horas = Read-Host "Ingresa las horas trabajadas"
+[double]$coste = Read-Host "Ingresa coste de horas"
+Write-Host "Tu salirio es:"
+[double]$resultado = $horas*$coste
+Write-Host $resultado
