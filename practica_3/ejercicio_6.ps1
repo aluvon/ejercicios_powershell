3cc6e3e85fb1fcd0f644bfadf2ed3b227c212316
aluvon <aluvon5089@ieselcaminas.org>
2023-09-19 20:21:14 +0200

    actualizados

--- practica_3/ejercicio_6.ps1
+++ practica_3/ejercicio_6.ps1
@@ -1,11 +1,12 @@
 ﻿Clear-Host
 Write-Host "Soy un buscador de archivos o directorios."
 $busqueda = Read-Host "Ingresa el nombre "
-$A = ($existe = Test-Path $busqueda) -and ($directorio = Test-Path $busqueda -PathType container)
-$B = ($existe -eq $true) -and ($directorio -eq $true)
+$A = ($existe = Test-Path $busqueda) -and ($es_directorio = Test-Path $busqueda -PathType container)
+$B = ($existe -eq $true) -and ($es_directorio -eq $true)
 If ($A -eq $B){
     Write-Host "`n"
-    Write-Host "Tus resultados de búsqueda son los siguientes" Get-ChildItem $busqueda -Recurse
+    Write-Host "El contenido es:" Get-Content "$busqueda"
+    Write-Host "Los subdirectorios son:" Get-ChildItem $busqueda -Recurse
 }
 Else{
     Write-Host "Fichero o Directorio no encontrado"
