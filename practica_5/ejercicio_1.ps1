4a2c753abc24b0539bb69509e7fe69873bace26b
aluvon <aluvon5089@ieselcaminas.org>
2023-10-02 09:12:48 +0200

    practica 5

---
+++ practica_5/ejercicio_1.ps1
@@ -0,0 +1,53 @@
+﻿#Funciones a utilizar en la calculadora
+Function Sumar {
+    param (
+        [int]$num1,
+        [int]$num2
+    )
+    $sumar = $num1 + $num2
+    Write-Host "El resultado de tu suma es: $sumar"
+    }
+Function Restar {
+    param (
+        [int]$num1,
+        [int]$num2
+    )
+    $resta = $num1 - $num2
+    Write-Host "El resultado de tu suma es: $resta"
+    }
+Function Multiplicar {
+    param (
+        [int]$num1,
+        [int]$num2
+    )
+    $multi = $num1 * $num2
+    Write-Host "El resultado de tu suma es: $multi"
+    }
+Function Division {
+    param (
+        [int]$num1,
+        [int]$num2
+    )
+    $div =$num1 / $num2
+    Write-Host "El resultado de tu suma es: $div"
+    }
+Clear-Host
+Write-Host "Con este script te pediré dos numeros, luego te preguntaré que operación quieres que realice entre ambos."
+[double]$num1 = Read-Host "Ingresa el primer número entero"
+[double]$num2 = Read-Host "Ingresa el segundo número entero"
+Write-Host "Elije una opción:"
+Write-Host "`n"
+Write-Host "SUMA=A, RESTA=B, DIVISIÓN=C, MULTIPLICACIÓN=D, SALIR=E"
+Write-Host "`n"
+$valores = Read-Host "Ingresa la opción deseada"
+Write-Host "`n"
+switch ($valores) {
+   A {Sumar $num1  $num2}
+   B {Restar $num1  $num2}
+   C {multiplicar $num1  $num2}
+   D {Division $num1  $num2}
+   E {Write-Host "Salir."}
+   default {Write-Host "Opción incorrecta."}
+}
+Write-Host "`n"
+Write-Host "Fin del script."
---
+++ practica_5/ejercicio_2.ps1
@@ -0,0 +1,6 @@
+﻿Clear-Host
+$personas = import-CSV usuarios.csv
+foreach($x in $personas) {
+    Write-Host "Plantilla turno día: $($x.nombre) $($x.apellidos) $($x.grupo)"
+}
+Write-Host "`n"
