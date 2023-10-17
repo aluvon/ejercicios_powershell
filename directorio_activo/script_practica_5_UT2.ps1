fb65d5ef8b54668e27d064f4186c1f4415101013
aluvon <aluvon5089@ieselcaminas.org>
2023-10-17 20:51:48 +0100

    practica_5_UT2

---
+++ directorio_activo/script_practica_5_UT2.ps1
@@ -0,0 +1,17 @@
+﻿Clear-Host
+Write-Host "Hola" 
+
+New-Item -Path C:\Empresa -ItemType directory
+
+# Creando los directorios y compartiendolos:
+$datos = Import-CSV C:\script2\departamentos.csv -Delimiter ";"
+foreach($x in $datos) {
+    $depto = $($x.departamento)
+    Write-Host $depto
+    New-Item -Path C:\Empresa\$depto -ItemType directory
+    New-SmbShare -Path C:\Empresa\$depto -Name $depto
+    Grant-SmbShareAccess -Name $depto -AccountName $depto -AccessRight Change -Force
+    Grant-SmbShareAccess -Name $depto -AccountName Administradores -AccessRight Full -Force
+    Grant-SmbShareAccess -Name $depto -AccountName "Usuarios del dominio" -AccessRight Read -Force
+    Revoke-SmbShareAccess -Name $depto -AccountName Todos -Force 
+}
