c20d777c67907accef36e1bc00a8105c3296e1ec
aluvon <aluvon5089@ieselcaminas.org>
2023-10-30 09:35:02 +0100

    practica 7

---
+++ directorio_activo/script_practica_7_UT2.ps1
@@ -0,0 +1,15 @@
+﻿Clear-Host
+Write-Host "Crearemos el perfil móvil para cada usuario" 
+
+
+# Verificando si el empleado pertenece al departamento Personal y le ponemos el perfil móvil.
+$datos = Import-CSV C:\script4\empleados.csv -Delimiter ";"
+foreach($x in $datos) {
+    $nombre = "$($x.nombre).$($x.apellido)"
+    $depto = "$($x.departamento)"
+
+    if ($depto -ieq "PERSONAL"){
+    Write-Host $nombre
+    Set-ADUser -Identity $nombre -ProfilePath \\Empresa-dc1\Empresa_user$\$nombre  
+    }
+}
