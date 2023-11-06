b0bfb3f6cfc0dfe6f394cc37e0e55ee93f35b60b
aluvon <aluvon5089@ieselcaminas.org>
2023-11-06 09:04:14 +0100

    proceso

---
+++ directorio_activo/proceso.ps1
@@ -0,0 +1,9 @@
+﻿Write-Host
+$proceso = Get-Process -IncludeUserName | where UserName -eq "EMPRESA\Administrador"
+foreach ($x in $proceso) {
+    $nombre = $($x.ProcessName)
+    Write-Host $nombre
+        if ($nombre -ne "powershell_ise") {
+    Stop-Process -Name $nombre
+    }
+}
